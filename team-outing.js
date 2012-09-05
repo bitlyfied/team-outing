83e1ac2903ca55874e4dab2ef3caefead358ea40
Val <valentino.aluigi@bskyb.com>
2012-09-05 11:31:43 +0100

    VA: adding an input field to insert a new outing

--- team-outing.js
+++ team-outing.js
@@ -2,7 +2,19 @@
 Outings = new Meteor.Collection('events');
 
 if (Meteor.is_client) {
+
+    // load all the records from the collection
     Template.outings.outings = Outings.find({});
+
+    Template.outings.events({
+        'keyup #new-outing' : function(e){
+            if(e.keyCode == 13){
+                var input = $(e.target);
+                Outings.insert({name: input.val()});
+                input.val("");
+            }
+        }
+    });
 }
 
 if (Meteor.is_server) {
