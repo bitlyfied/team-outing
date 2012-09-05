6ad56449e5df6da12be4d3675912d01914f28fcd
Val <valentino.aluigi@bskyb.com>
2012-09-05 11:20:22 +0100

    VA: removing hello world stuff

--- team-outing.js
+++ team-outing.js
@@ -1,20 +1,12 @@
-outings = new Meteor.Collection('events'); 
-if (Meteor.is_client) {
-  Template.hello.greeting = function () {
-    return "Welcome to team-outing.";
-  };
+// Models
+Outings = new Meteor.Collection('events');
 
-  Template.hello.events = {
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  };
-  Template.hello.outings = outings.find({});
-};
+if (Meteor.is_client) {
+    Template.outings.outings = Outings.find({});
+}
+
 if (Meteor.is_server) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
